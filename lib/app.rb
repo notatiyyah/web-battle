cf2a6326b8ff7d5afab112b063d8bbda3af67563
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-07 17:12:54 +0000

    Entering player names

--- lib/app.rb
+++ lib/app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class BattleApp < Sinatra::Base
 
   get '/' do
-    "Testing infrastructure working!"
+    erb :index
+  end
+
+  post '/names' do
+    @P1 = params["player_1"]
+    @P2 = params["player_2"]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/infrastructure_test_spec.rb
+++ spec/features/infrastructure_test_spec.rb
@@ -3,8 +3,12 @@ require_relative "../../lib/app"
 require 'spec_helper'
 
 feature 'Testing infrastructure' do
-    scenario 'Can run app and check page content' do
-      visit('/')
-      expect(page).to have_content 'Testing infrastructure working!'
-    end
+
+  scenario 'Allow users to input names' do
+    visit('/')
+    fill_in "player_1", with: "P1"
+    fill_in "player_2", with: "P2"
+    click_on 'Submit'
+    expect(page).to have_content "P1 P2"
   end
+end
