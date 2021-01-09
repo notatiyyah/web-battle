6e728c77f060d4a787f045e9345a8e47c88a1c49
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-09 17:20:42 +0000

    Fix indentation

--- lib/game.rb
+++ lib/game.rb
@@ -1,33 +1,33 @@
 class Game
-    attr_reader :p1, :p2, :dead_players, :current_player
+  attr_reader :p1, :p2, :dead_players, :current_player
 
-    def initialize(p1,p2)
-      @p1 = p1
-      @p2 = p2
-      @dead_players = []
-      @current_player = @p1
-    end
+  def initialize(p1,p2)
+    @p1 = p1
+    @p2 = p2
+    @dead_players = []
+    @current_player = @p1
+  end
 
-    def launch_attack(player)
-      player == "P1_Attack" ? p1_take_turn : p2_take_turn
-      check_if_any_dead
-    end
+  def launch_attack(player)
+    player == "P1_Attack" ? p1_take_turn : p2_take_turn
+    check_if_any_dead
+  end
 
-    private
+  private
 
-    def p1_take_turn
-      @p2.change_hp(-10)
-      @current_player = @p2
-    end
+  def p1_take_turn
+    @p2.change_hp(-10)
+    @current_player = @p2
+  end
 
-    def p2_take_turn
-      @p1.change_hp(-10)
-      @current_player = @p1
-    end
+  def p2_take_turn
+    @p1.change_hp(-10)
+    @current_player = @p1
+  end
 
-    def check_if_any_dead
-      @dead_players << "Player_1" if @p1.dead?
-      @dead_players << "Player_2" if @p2.dead?
-    end
+  def check_if_any_dead
+    @dead_players << "Player_1" if @p1.dead?
+    @dead_players << "Player_2" if @p2.dead?
+  end
 
 end
